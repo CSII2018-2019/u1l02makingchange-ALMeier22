4ea6334efa8564f35c92800bc03547e6745f3f7d
antonia <antonia@172.16.21.136>
2018-09-13 08:40:31 -0400

    hi

--- src/U1L02MakingChange.java
+++ src/U1L02MakingChange.java
@@ -1,15 +1,4 @@
 
-// pseudo-code
-		// .98 -.05-.05-.05... 
-			// until <.05
-	// Use a while loop
-		// while (# is still over .05)
-			// count. going through out 
-			// else count remaning as penny
-	// print how many times it went throguh the frist part of the loop for
-         //nickles
-	// print how much was left over for pennies
-
 public class U1L02MakingChange {
 
 	public static void main(String[] args) {
