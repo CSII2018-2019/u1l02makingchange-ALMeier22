d6187560c1bac45144fbdb8fe1d84f80a686acfc
antonia <antonia@antonias-air.lan>
2018-09-15 08:35:23 -0400

    Making Change: dollars, quarters, dimes, nickels, and pennies

--- src/U1L02MakingChange.java
+++ src/U1L02MakingChange.java
@@ -1,9 +1,61 @@
+import javax.swing.JOptionPane;
 
 public class U1L02MakingChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//kjjh
+		
+		//using a loop
+		
+		// define variables for the amounts for each coin
+		int nickels = 0;
+		int pennies = 0;
+		int dimes = 0;
+		int quarters = 0;
+		int dollars = 0;
+		// Get input from user (change to usable value)
+		String answer = JOptionPane.showInputDialog("Ener a"
+				+ " monitary value (ex: 1.56)");
+		double cash = Double.parseDouble(answer);
+		pennies = (int)(cash * 100);
+		//System.out.println(pennies);
+	
+		// loop subtracting coin amount until money < coin amount 
+		
+		//count dollars 
+		while (pennies > 100) {
+			dollars++;
+			pennies -= 100;
+		}
+		
+		// count quarters
+		while (pennies > 25) {
+			quarters++;
+			pennies -= 25;
+		}
+		// count dimes
+		while (pennies > 10) {
+			dimes++;
+			pennies -= 10;
+		}
+
+		// count nickels
+		while (pennies > 5) {
+			nickels++;
+			pennies -= 5;
+		}
+		
+		//pennies = money * 100 < don't need this b/c 
+		//converted to an integer vale 
+	
+		
+		// print money 
+		JOptionPane.showMessageDialog(null,"$" + cash + " is "
+				+ dollars + " dollars "
+				+ quarters + " quarters "
+				+ dimes + " dimes, "
+				+ nickels + " nickels and "
+				+ pennies + " pennies.") ;
+		
 	}
 
 }
